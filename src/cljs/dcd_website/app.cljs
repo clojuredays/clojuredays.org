334fe3462f719d1850669f79f8fcc187abe42369
Max Gonzih <gonzih@gmail.com>
2016-11-03 11:38:30 +0100

    Initial website template with dummy text

--- src/cljs/dcd_website/app.cljs
+++ src/cljs/dcd_website/app.cljs
@@ -1,18 +1,88 @@
 (ns dcd-website.app
   (:require [reagent.core :as reagent :refer [atom]]))
 
-(defn some-component []
-  [:div
-   [:h3 "I am a component!"]
-   [:p.someclass
-    "I have " [:strong "bold"]
-    [:span {:style {:color "red"}} " and red"]
-    " text."]])
+(def dcd-team
+  [["Carlo Sciolla" "skuro"]
+   ["Vijay Kiran" "vijaykiran"]
+   ["Max Gonzih" "gonzih"]
+   ["Joost Diepenmaat" "ZeekatSoftware"]])
+
+(def lorem-str "Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing.")
+
+(defn lorem [n]
+  (apply str (take n (repeat lorem-str))))
+
+(defn header-component []
+  [:header.header
+   [:div.logo
+    [:img {:src "/img/logo_big2.png"}]]
+   [:div.title
+    [:h1 "Dutch Clojure Days 2017"]]])
+
+(defn sidebar-component []
+  [:aside.sidebar
+   [:a.item {:href "#about"} "About"]
+   [:a.item {:href "#date"} "When?"]
+   [:a.item {:href "#location"} "Where?"]
+   [:a.item {:href "#cfp"} "Call for papers"]
+   [:a.item {:href "#behave"} "Code of conduct"]
+   [:a.item {:href "#org"} "Organizers"]])
+
+(defn main-component []
+  [:article.main
+   [:div#about
+    [:h2 "About DCD17"]
+    [:p "The Annual Gathering of Clojure Enthusiasts and Practitioners in the Netherlands!"]
+    [:p "Videos from the previous event are available here: "
+     [:a {:href "https://www.youtube.com/playlist?list=PLjzcwcP9P2LeiVXofnnXcxAqvmVNn3fTD"
+          :target :_blank}
+      "DCD 2016"]]]
+   [:div#date
+    [:h2 "When?"]
+    [:p (lorem 1)]]
+   [:div#location
+    [:h2 "Where?"]
+    [:p (lorem 2)]]
+   [:div#cfp
+    [:h2 "Call for papers"]
+    [:p (lorem 1)]]
+   [:div#behave
+    [:h2 "Code of conduct"]
+    [:p (lorem 5)]]
+   [:div#org
+    [:h2 "Organizers"]
+    [:h3 "DCD team"]
+    [:div
+     (letfn [(organizer [[name handle]]
+               ^{:key handle}
+               [:span.org-info " " name " "
+                "("
+                [:a {:href (str "https://twitter.com/" handle)
+                     :target :_blank}
+                 (str "@" handle)]
+                ")"])]
+       (map organizer dcd-team))]
+    [:h3 "Design by"]
+    "Lubov Soltan ("
+    [:a {:href "https://twitter.com/moolver_sin"}
+     "@moolver_sin"] ")"]])
+
+(defn footer-component []
+  [:footer.footer
+   [:a {:href "https://twitter.com/clojuredays"
+        :target :_blank}
+    "@clojuredays"]
+   [:a {:href "mailto:contact@clojuredays.org"}
+    "contact@clojuredays.org"]])
 
 (defn calling-component []
-  [:div "Parent component"
-   [some-component]])
+  [:div.wrapper
+   [header-component]
+   [sidebar-component]
+   [main-component]
+   [footer-component]])
 
 (defn init []
-  (reagent/render-component [calling-component]
-                            (.getElementById js/document "container")))
+  (reagent/render-component
+   [calling-component]
+   (.getElementById js/document "container")))
