897da12511b5eed641a9cafc54842fec997fdfb8
Max Gonzih <gonzih@gmail.com>
2016-11-07 15:59:44 +0100

    Change contact email

--- src/cljs/dcd_website/app.cljs
+++ src/cljs/dcd_website/app.cljs
@@ -76,7 +76,7 @@
    [:a {:href "https://www.youtube.com/channel/UCZ7rYtyjSgF1jMYHCkyjwMQ"
         :target :_blank}
     [:img {:src "img/icons/youtube.svg"}]]
-   [:a {:href "mailto:contact@clojuredays.org"}
+   [:a {:href "mailto:events@clojuredays.org"}
     [:img {:src "img/icons/email.svg"}]]])
 
 (defn calling-component []
