0cc3c240355f12ab534745f4afdb07bd29fb84cb
Max Gonzih <gonzih@gmail.com>
2017-02-11 20:24:28 +0100

    Fixes

--- src/cljs/dcd_website/app.cljs
+++ src/cljs/dcd_website/app.cljs
@@ -280,7 +280,7 @@
 
    {:time ["14:15" "14:30"]
     :title "Sequencing Dance Music with Clojure"
-    :profile-pic "piotr.png"
+    :profile-pic "piotr.jpg"
     :description "In this talk I'll give a short introduction to Overtone, Leipzig and my own Disclojure libraries. I'll focus on techniques of sequencing and looping sounds and samples which can be used in live on-stage dance music performances."
     :author "Piotr Jagielski"
     :bio "JVM/Java developer for 10 years, last 2 years exploring Clojure/Overtone."
@@ -307,7 +307,7 @@
 
    {:time ["15:00" "15:15"]
     :title "CREPL: Write and run ClojureScript code together"
-    :profile-pic "gijs.png"
+    :profile-pic "gijs.jpg"
     :author "Gijs Stuurman"
     :description "CREPL is an online collaborative ClojureScript editor and evaluator. It's like a Google docs for ClojureScript, with a self-hosted ClojureScript compiler. This talks shows how to use CREPL to work together on the same ClojureScript code, for instance in a teaching setting. This talk is also a demonstration of what is possible with Clojure and ClojureScript because CREPL is written with both."
     :bio "Clojure programmer in Amsterdam."
