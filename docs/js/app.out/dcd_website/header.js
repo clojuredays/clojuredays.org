fb0987ed81b9f6395ebf1a7f7975c982138aaf2a
Vijay Kiran <mail@vijaykiran.com>
2017-11-30 17:22:07 +0100

    Website updated on Thu Nov 30 17:21:39 CET 2017

--- docs/js/app.out/dcd_website/header.js
+++ docs/js/app.out/dcd_website/header.js
@@ -3,14 +3,14 @@ goog.provide('dcd_website.header');
 goog.require('cljs.core');
 dcd_website.header.header_component = (function dcd_website$header$header_component(var_args){
 var args__7522__auto__ = [];
-var len__7515__auto___12048 = arguments.length;
-var i__7516__auto___12049 = (0);
+var len__7515__auto___12634 = arguments.length;
+var i__7516__auto___12635 = (0);
 while(true){
-if((i__7516__auto___12049 < len__7515__auto___12048)){
-args__7522__auto__.push((arguments[i__7516__auto___12049]));
+if((i__7516__auto___12635 < len__7515__auto___12634)){
+args__7522__auto__.push((arguments[i__7516__auto___12635]));
 
-var G__12050 = (i__7516__auto___12049 + (1));
-i__7516__auto___12049 = G__12050;
+var G__12636 = (i__7516__auto___12635 + (1));
+i__7516__auto___12635 = G__12636;
 continue;
 } else {
 }
@@ -21,18 +21,18 @@ var argseq__7523__auto__ = ((((1) < args__7522__auto__.length))?(new cljs.core.I
 return dcd_website.header.header_component.cljs$core$IFn$_invoke$arity$variadic((arguments[(0)]),argseq__7523__auto__);
 });
 
-dcd_website.header.header_component.cljs$core$IFn$_invoke$arity$variadic = (function (p__12045,children){
-var map__12046 = p__12045;
-var map__12046__$1 = ((((!((map__12046 == null)))?((((map__12046.cljs$lang$protocol_mask$partition0$ & (64))) || (map__12046.cljs$core$ISeq$))?true:false):false))?cljs.core.apply.cljs$core$IFn$_invoke$arity$2(cljs.core.hash_map,map__12046):map__12046);
-var styles = cljs.core.get.cljs$core$IFn$_invoke$arity$2(map__12046__$1,cljs.core.cst$kw$styles);
-var title = cljs.core.get.cljs$core$IFn$_invoke$arity$2(map__12046__$1,cljs.core.cst$kw$title);
+dcd_website.header.header_component.cljs$core$IFn$_invoke$arity$variadic = (function (p__12631,children){
+var map__12632 = p__12631;
+var map__12632__$1 = ((((!((map__12632 == null)))?((((map__12632.cljs$lang$protocol_mask$partition0$ & (64))) || (map__12632.cljs$core$ISeq$))?true:false):false))?cljs.core.apply.cljs$core$IFn$_invoke$arity$2(cljs.core.hash_map,map__12632):map__12632);
+var styles = cljs.core.get.cljs$core$IFn$_invoke$arity$2(map__12632__$1,cljs.core.cst$kw$styles);
+var title = cljs.core.get.cljs$core$IFn$_invoke$arity$2(map__12632__$1,cljs.core.cst$kw$title);
 return new cljs.core.PersistentVector(null, 5, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$header$header,styles,new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$div$logo,new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$img,new cljs.core.PersistentArrayMap(null, 1, [cljs.core.cst$kw$src,"img/logo.svg"], null)], null)], null),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$div$title,new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$h1,title], null)], null),children], null);
 });
 
 dcd_website.header.header_component.cljs$lang$maxFixedArity = (1);
 
-dcd_website.header.header_component.cljs$lang$applyTo = (function (seq12043){
-var G__12044 = cljs.core.first(seq12043);
-var seq12043__$1 = cljs.core.next(seq12043);
-return dcd_website.header.header_component.cljs$core$IFn$_invoke$arity$variadic(G__12044,seq12043__$1);
+dcd_website.header.header_component.cljs$lang$applyTo = (function (seq12629){
+var G__12630 = cljs.core.first(seq12629);
+var seq12629__$1 = cljs.core.next(seq12629);
+return dcd_website.header.header_component.cljs$core$IFn$_invoke$arity$variadic(G__12630,seq12629__$1);
 });
