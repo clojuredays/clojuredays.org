6a6e645d602fc921783710e0261383d664c35fde
Carlo Sciolla <carlo.sciolla@gmail.com>
2018-02-12 22:54:09 +0100

    invert logo and title for a cleaner look

--- src/cljs/dcd_website/header.cljs
+++ src/cljs/dcd_website/header.cljs
@@ -3,8 +3,8 @@
 (defn header-component
   [{:keys [styles title]} & children]
   [:header.header styles
-   [:div.logo
-    [:img {:src "img/2018/logo.svg"}]]
    [:div.title
     [:h1 title]]
+   [:div.logo
+    [:img {:src "img/2018/logo.svg"}]]
    children])
