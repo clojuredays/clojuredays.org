b11710b059b1f59c8e0b30f5af0d754757ee0e5d
Vijay Kiran <mail@vijaykiran.com>
2018-11-16 12:50:01 +0100

    Website updated on Fri Nov 16 12:49:21 CET 2018

--- src/cljs/dcd_website/header.cljs
+++ src/cljs/dcd_website/header.cljs
@@ -4,6 +4,6 @@
   [{:keys [styles title]} & children]
   [:header.header styles
    [:div.logo
-    [:img {:src "img/2019/logo-200.png"}]]
+    [:img {:src "img/2019/dcd19-logo.png"}]]
    [:h1 [:div.title title]]
    children])
