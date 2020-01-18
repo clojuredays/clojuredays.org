0009edd1bb6c13e62e539f30ed8bb981fe2dcdae
Carlo Sciolla <carlo.sciolla@gmail.com>
2020-01-18 22:00:52 +0100

    Continuously build CSS and serve HTML from the public folder

---
+++ scripts/dev.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Start a webserver serving the static resources in the background and capture the PID
+python3 -m http.server --directory public/ > /dev/null &
+SERVER="$!"
+
+# Recompile CSS on save
+lein sass watch &
+SASS="$!"
+
+echo "Hit <enter> to quit"
+read DONE
+
+kill "$SERVER"
+trap "kill $SERVER $SASS" SIGINT SIGTERM EXIT SIGSTOP
+
+echo Done.
