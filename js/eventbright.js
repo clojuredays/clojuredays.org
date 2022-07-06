b7c4cc342897da31128d948feb7d2ddbf08b01d8
skuro <skuro@users.noreply.github.com>
2022-07-06 21:33:47 +0000

    deploy: 58e48d58dc0597ba3e6301e2e80895fd1479e630

--- js/eventbright.js
+++ js/eventbright.js
@@ -1,4 +1,4 @@
-const exampleCallback = function() {
+var exampleCallback = function() {
   console.log('Order complete!');
 };
 
@@ -6,9 +6,10 @@ window.EBWidgets.createWidget({
   // Required
   widgetType: 'checkout',
   eventId: '380638669507',
-  iframeContainerId: 'eventbrite-widget-container-380638669507',
+  modal: true,
+  modalTriggerElementId: 'eventbrite-widget-modal-trigger-380638669507',
 
   // Optional
   iframeContainerHeight: 425,  // Widget height in pixels. Defaults to a minimum of 425px if not provided
-  onOrderComplete: exampleCallback  // Method called when an order has successfully completed
+  onOrderComplete: exampleCallback
 });
